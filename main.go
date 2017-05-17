a4a4130406e296355393e9524cec5c4c518728bc
zhouzhou <v_zhou@outlook.com>
2017-05-17 17:59:13 +0800

    git submodule

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
-package main 
+package main
+
+import (
+	"fmt"
+
+	"time"
+
+	"dsp.ohko.cn/zhouzhou/vcommon/goroutinepool"
+)
 
 func main() {
-	
+	goPool := goroutinepool.NewPool(10, 100)
+	goPool.Start()
+
+	goPool.AddTask(func() {
+		fmt.Println("test")
+	})
+
+	time.Sleep(time.Second)
+
 }
